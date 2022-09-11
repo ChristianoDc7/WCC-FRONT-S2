444ac766cb743d97acd02d81f460e14b7c2406e1
Christiano David <christianod97@gmail.com>
2022-09-11 08:35:18 +0300

    profile

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,50 @@
-import React from 'react';
-import './App.css';
-import './styles/common.scss'
-import './styles/navbar.scss'
+import React from "react";
+import "./App.css";
+import "./styles/common.scss";
+import "./styles/navbar.scss";
 
-import { SwiperSection } from './components/SwiperSection';
-import { RouteCard } from './components/RouteCard';
-import { Compass } from './widget/Compass';
-import { Curve } from './widget/Curve';
-import { Route } from './widget/Route';
+import { SwiperSection } from "./components/SwiperSection";
+import { RouteCard } from "./components/RouteCard";
+import { Compass } from "./widget/Compass";
+import { Curve } from "./widget/Curve";
+import { Route } from "./widget/Route";
+import { Profile } from "./components/Profile";
 
 function App() {
   return (
     <div className="App">
-       <div className="card">
-            <div className="header">
-              <div className="logo">Wingle</div>
-            </div>
-            <div className="main">
-              <nav>
-                  <a href="#">Popular</a>
-                  <a href="#">Recommended</a>
-                  <a href="#">Extreme</a>
-                  <div className="animation start-home"></div>
-              </nav>
-              <SwiperSection />
-            </div>
-            <div style={{display:"flex", columnGap:"15px"}}>
-              <RouteCard title='Camping guide' icon={<Compass height='73px' width='73px'/>}/>
-              <RouteCard title='Dangerous hikes' icon={<Route height='73px' width='73px'/>}/>
-            </div>
-            
-            <Curve height='100px'/>
-            <Curve height='100px' rotation='90'/>
-       </div>
+      <div className="card">
+        <div className="left">
+          <div className="header">
+            <div className="logo">Wingle</div>
+          </div>
+          <div className="main">
+            <nav>
+              <a href="#">Popular</a>
+              <a href="#">Recommended</a>
+              <a href="#">Extreme</a>
+              <div className="animation start-home"></div>
+            </nav>
+            <SwiperSection />
+          </div>
+          <div style={{ display: "flex", columnGap: "15px" }}>
+            <RouteCard
+              title="Camping guide"
+              icon={<Compass height="73px" width="73px" />}
+            />
+            <RouteCard
+              title="Dangerous hikes"
+              icon={<Route height="73px" width="73px" />}
+            />
+          </div>
+
+          {/* <Curve height="100px" />
+          <Curve height="100px" rotation="90" /> */}
+        </div>
+        <div className="right">
+            <Profile />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Profile.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Profile = () => {
+  return (
+    <div className='profile'>
+        <div className="pdp">
+
+        </div>
+        <div className="infos">
+            <h2>Helena Brauer</h2>
+            <p>Manager</p>
+            <p>helena@brauer.com</p>
+        </div>
+    </div>
+  )
+}
